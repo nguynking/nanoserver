85aa0fc6ea059b1fed08f563179d1342b6a76499
nguynking <kingnguyen0123@gmail.com>
2026-02-04 11:52:15 +0700

    write logic for sending DELETE request to the server:
    - accept the command line option `-X <method>` that allow us to specify the HTTP method.
    - changing the method sent to DELETE.

--- curl.py
+++ curl.py
@@ -1,35 +1,17 @@
 from urllib.parse import urlparse
 import socket
 import argparse
-import json
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Curl-like tool")
-    parser.add_argument("url", type=str, help="URL to fetch")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
-    args = parser.parse_args()
-
-    url = urlparse(args.url)
-    host = url.hostname
-    port = url.port if url.port else 80
-    path = url.path if url.path else "/"
-
-    if host is None:
-        print("Invalid URL")
-        exit(1)
-    if url.scheme != "http":
-        print("Only HTTP is supported")
-        exit(1)
-
+def send_request(host, port, path, method, verbose):
     request_headers = [
-        f"GET {path} HTTP/1.1",
+        f"{method} {path} HTTP/1.1",
         f"Host: {host}",
         "Accept: */*",
         "Connection: close"
     ]
     request = "\r\n".join(request_headers) + "\r\n\r\n"
 
-    if args.verbose:
+    if verbose:
         print("> " + "\n> ".join(request_headers) + "\n>")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -42,7 +24,32 @@ if __name__ == "__main__":
                 break
             response += chunk
         response_headers, response_data = response.decode('utf-8').split("\r\n\r\n", 1)
-        if args.verbose:
+        if verbose:
             response_header_section = response_headers.split("\r\n")
             print("< " + "\n< ".join(response_header_section) + "\n<")
-        print(response_data) 
+        print(response_data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Curl-like tool")
+    parser.add_argument("url", type=str, help="URL to fetch")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+    parser.add_argument("-X", "--method", type=str, default="GET", help="HTTP method to use")
+    args = parser.parse_args()
+
+    url = urlparse(args.url)
+    host = url.hostname
+    port = url.port if url.port else 80
+    path = url.path if url.path else "/"
+
+    if host is None:
+        print("Invalid URL")
+        exit(1)
+    if args.method not in ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"]:
+        print("Invalid HTTP method")
+        exit(1)
+    if url.scheme != "http":
+        print("Only HTTP is supported")
+        exit(1)
+
+    send_request(host, port, path, args.method, args.verbose)
